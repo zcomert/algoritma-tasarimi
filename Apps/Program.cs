1d5f458ed0f0aa70fded19bd8d1eea001d88bfaa
Zafer CÖMERT <zcomert@samsun.edu.tr>
2020-12-08 15:19:59 +0300

    Mutlak Değer Fonksiyonu eklendi.

--- Apps/Program.cs
+++ Apps/Program.cs
@@ -10,23 +10,39 @@ namespace Apps
     {
         static void Main(string[] args)
         {
+            int[] X = new int[] { -5, 10, -20, 30, -23 };
+            for (int i = 0; i < X.Length; i++)
+            {
+                X[i] = MutlakDeger(X[i]);
+                Console.WriteLine(X[i]);
+            }
+            Console.ReadKey();
+        }
+        public static int MutlakDeger(int n)
+        {
+            if (n < 0)
+                n = -n;
+            return n;
+        }
+        #region Ornekler
+        private static void TekCiftSayiOrnegi()
+        {
             // ekran cikti verir. 
             Console.WriteLine("Bir sayi giriniz: ");
-            
+
             // klavyeden girilen bir sayiyi saiy degiskenin de saklar. 
-            int sayi = 
+            int sayi =
                 Convert.ToInt32(Console.ReadLine());
 
             // fonksiyon sonucunu sonuc degiskeninde saklar. 
             bool sonuc = TekMi(sayi);
-            
+
             // Sonucu ekrana yazar. 
             Console.WriteLine($"Tek mi? {sonuc}");
-            
+
             // Klavyeden bir tusa basılması bekler. 
             Console.ReadKey();
         }
-
         /// <summary>
         /// Parametre olarak aldgini sayinin tek mi yoksa cift mi oldugunu kontrol eder.
         /// </summary>
@@ -41,7 +57,6 @@ namespace Apps
             else
                 return false;
         }
-
         private static void NKadarYazdir(int n=5)
         {
             Console.WriteLine();
@@ -51,8 +66,22 @@ namespace Apps
             }
             Console.WriteLine();
         }
-        
+        private static void MutlakDegerOrnegi()
+        {
+            int n = Convert
+                .ToInt32(Console.ReadLine());
 
-        
+            if (n >= 0)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine(-n);
+            }
+
+            Console.ReadKey();
+        }
+        #endregion
     }
 }
